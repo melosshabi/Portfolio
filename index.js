3fb335c48b9171e5aef80c1d3c44505b6dbf70fc
melosshabi <melosshabi05@gmail.com>
2023-02-22 20:39:12 +0100

    New Portfolio

---
+++ index.js
@@ -0,0 +1,26 @@
+let expandSidebarBtn = document.getElementsByClassName('hamburger-btn')[0];
+let shrinkSidebarBtn = document.getElementsByClassName('close-hamburger-btn')[0];
+let mobileSidebar = document.getElementsByClassName('mobile-sidebar')[0];
+let mobileSidebarLinks = document.querySelectorAll('.mobile-sidebar a');
+let darkDiv = document.getElementsByClassName('dark-div')[0];
+let body = document.querySelector('body');
+
+
+expandSidebarBtn.addEventListener('click', () =>{
+   mobileSidebar.classList.add('active')
+   darkDiv.classList.add('activeDark')
+   body.style.overflow = "hidden";
+})
+shrinkSidebarBtn.addEventListener('click', () =>{
+    mobileSidebar.classList.toggle('active')
+    darkDiv.classList.remove('activeDark')
+    body.style.overflow = "visible";
+})
+
+for(let i = 0; i < mobileSidebarLinks.length; i++){
+    mobileSidebarLinks[i].addEventListener('click', ()=>{
+        mobileSidebar.classList.remove('active')
+        darkDiv.classList.remove('activeDark');
+        body.style.overflow = "visible";
+    })
+}
