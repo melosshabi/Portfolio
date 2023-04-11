1936dc838c911a8eb50bcd30a1bfaffe999b15bb
Melos Shabi <melosshabi05@gmail.com>
2023-04-11 09:04:57 +0200

    Update

--- index.js
+++ index.js
@@ -3,24 +3,22 @@ let shrinkSidebarBtn = document.getElementsByClassName('close-hamburger-btn')[0]
 let mobileSidebar = document.getElementsByClassName('mobile-sidebar')[0];
 let mobileSidebarLinks = document.querySelectorAll('.mobile-sidebar a');
 let darkDiv = document.getElementsByClassName('dark-div')[0];
-let body = document.querySelector('body');
-
 
 expandSidebarBtn.addEventListener('click', () =>{
    mobileSidebar.classList.add('active-mobile-sidebar')
    darkDiv.classList.add('activeDark')
-   body.style.overflow = "hidden";
+   document.documentElement.style.overflow = "hidden";
 })
 shrinkSidebarBtn.addEventListener('click', () =>{
     mobileSidebar.classList.toggle('active-mobile-sidebar')
     darkDiv.classList.remove('activeDark')
-    body.style.overflow = "visible";
+    document.documentElement.style.overflowY = "visible";
 })
 
 for(let i = 0; i < mobileSidebarLinks.length; i++){
     mobileSidebarLinks[i].addEventListener('click', ()=>{
-        mobileSidebar.classList.remove('active')
+        mobileSidebar.classList.remove('active-mobile-sidebar')
         darkDiv.classList.remove('activeDark');
-        body.style.overflow = "visible";
+        document.documentElement.style.overflowY = 'scroll'
     })
 }
